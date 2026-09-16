d583f5db141eef751e67cdc3422d7205c9a5455d
Elias Rinne <elias1rinne@gmail.com>
2026-09-16 10:30:14 +0300

    Initial commit

---
+++ src/main/java/com/api_vendor/vendor_main/VendorMainApplication.java
@@ -0,0 +1,13 @@
+package com.api_vendor.vendor_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_vendor/vendor_main/VendorMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_vendor.vendor_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
